9df7631a8103bf4c4be6445993cac5884a511b82
Cristian <35327838+01fbk@users.noreply.github.com>
2019-05-24 16:21:04 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+
+//Set access tokens here - see: https://dev.twitter.com/apps/ 
+$oauth_access_token = "";
+$oauth_access_token_secret = "";
+$consumer_key = "";
+$consumer_secret = "";
+$twitter_user = "Your twitter username goes here.";
+
+?>
